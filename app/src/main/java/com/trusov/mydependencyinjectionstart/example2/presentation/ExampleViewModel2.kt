eae8e1ff3f4e322a9f7c3c1ca692d902468e0774
Glebasta333 <gleb.trusow@yandex.ru>
2022-01-25 12:44:40 +0300

    add second view model to factory

--- app/src/main/java/com/trusov/mydependencyinjectionstart/example2/presentation/ExampleViewModel2.kt
+++ app/src/main/java/com/trusov/mydependencyinjectionstart/example2/presentation/ExampleViewModel2.kt
@@ -2,15 +2,15 @@ package com.trusov.mydependencyinjectionstart.example2.presentation
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.trusov.mydependencyinjectionstart.example2.domain.ExampleUseCase
+import com.trusov.mydependencyinjectionstart.example2.domain.ExampleRepository
 import javax.inject.Inject
 
-class ExampleViewModel @Inject constructor(
-    private val useCase: ExampleUseCase
+class ExampleViewModel2 @Inject constructor(
+    private val repository: ExampleRepository
 ) : ViewModel() {
 
     fun method() {
-        useCase()
+        repository.method()
         Log.d("ExampleViewModel", "$this")
     }
 }
--- app/src/main/java/com/trusov/mydependencyinjectionstart/example2/presentation/ViewModelFactory.kt
+++ app/src/main/java/com/trusov/mydependencyinjectionstart/example2/presentation/ViewModelFactory.kt
@@ -2,18 +2,20 @@ package com.trusov.mydependencyinjectionstart.example2.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.trusov.mydependencyinjectionstart.example2.domain.ExampleRepository
 import com.trusov.mydependencyinjectionstart.example2.domain.ExampleUseCase
 import java.lang.RuntimeException
 import javax.inject.Inject
 
 class ViewModelFactory @Inject constructor(
-    private val useCase: ExampleUseCase
+    private val useCase: ExampleUseCase,
+    private val repository: ExampleRepository
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        if(modelClass == ExampleViewModel::class.java){
-            return ExampleViewModel(useCase) as T
-        } else {
-            throw RuntimeException("Unknown view model class $modelClass")
+        return when (modelClass) {
+            ExampleViewModel::class.java -> ExampleViewModel(useCase) as T
+            ExampleViewModel2::class.java -> ExampleViewModel2(repository) as T
+            else -> throw RuntimeException("Unknown view model class $modelClass")
         }
     }
 }
