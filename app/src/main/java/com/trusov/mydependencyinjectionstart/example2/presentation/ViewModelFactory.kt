23a4d5792d66fe81fb3344b0777a049b127a94f4
Glebasta333 <gleb.trusow@yandex.ru>
2022-01-26 11:23:01 +0300

    add ViewModelKey annotation

--- app/src/main/java/com/trusov/mydependencyinjectionstart/example2/presentation/ViewModelFactory.kt
+++ app/src/main/java/com/trusov/mydependencyinjectionstart/example2/presentation/ViewModelFactory.kt
@@ -5,12 +5,12 @@ import androidx.lifecycle.ViewModelProvider
 import com.trusov.mydependencyinjectionstart.example2.di.ApplicationScope
 import javax.inject.Inject
 import javax.inject.Provider
-// Как сделать, чтоб viewModelFactory был синглтоном, но не сами ViewModel?
+
 @ApplicationScope
 class ViewModelFactory @Inject constructor(
-    private val viewModelProviders: @JvmSuppressWildcards Map<String, Provider<ViewModel>> // Provider - обёртка (будет возращяться один и тот же, а ViewModel будут создаваться новые)
+    private val viewModelProviders: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return viewModelProviders[modelClass.simpleName]?.get() as T
+        return viewModelProviders[modelClass]?.get() as T
     }
 }
