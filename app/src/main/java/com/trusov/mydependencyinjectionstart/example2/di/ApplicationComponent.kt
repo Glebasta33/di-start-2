50a5228a5bc1158aed94d85e241f7cd762f12f11
Glebasta333 <gleb.trusow@yandex.ru>
2022-01-27 13:27:59 +0300

    add subcomponent

--- app/src/main/java/com/trusov/mydependencyinjectionstart/example2/di/ApplicationComponent.kt
+++ app/src/main/java/com/trusov/mydependencyinjectionstart/example2/di/ApplicationComponent.kt
@@ -5,14 +5,12 @@ import com.trusov.mydependencyinjectionstart.example2.presentation.MainActivity
 import com.trusov.mydependencyinjectionstart.example2.presentation.MainActivity2
 import dagger.BindsInstance
 import dagger.Component
-import javax.inject.Singleton
 
 @ApplicationScope
-@Component(modules = [DomainModule::class, DataModule::class, ViewModelModule::class])
+@Component(modules = [DomainModule::class, DataModule::class])
 interface ApplicationComponent {
 
-    fun inject(activity: MainActivity)
-    fun inject(activity: MainActivity2)
+    fun activityComponentFactory(): ActivityComponent.Factory
 
     @Component.Factory
     interface ApplicationComponentFactory {
